cbf47633195426bb5ef0f4f42a5783e0e55610d4
pedroooliveira25 <eu.pedrohfo@gmail.com>
2026-05-27 11:28:46 -0300

    Definindo escopo básico: DataAddress

--- WebApplication/src/Domain/Entities/DataAddress.cs
+++ WebApplication/src/Domain/Entities/DataAddress.cs
@@ -1,24 +1,62 @@
-public class DataAddress
+using System.ComponentModel.DataAnnotations.Schema;
+public class DataAddress : Notification
 {
-    private string Road {get; set;}
-    private int Number {get; set;}
-    private string District {get; set;}
-    private string City {get; set;}
-    private string Stage {get; set;}
-    private string Cep {get; set;}
+    [Column("Road")]
+    private string Road { get; set; }
+    [Column("Number")]
+    private int Number { get; set; }
+    [Column("District")]
+    private string District { get; set; }
+    [Column("City")]
+    private string City { get; set; }
+    [Column("Stage")]
+    private string Stage { get; set; }
+    [Column("Cep")]
+    private int Cep { get; set; }
 
 
 
-    public DataAddress(string road, int number, string district, string city, string stage, string cep )
+    public DataAddress(string road, string district, string city, string stage, int cep, int number)
     {
-        
-        
 
 
-        this.Road = road;
-        this.Number = number;
-        this.District = district;
-        this.City = city;
-        this.Stage = cep;
+        ValidatePropertiesString(road, "Road");
+        ValidatePropertiesString(district, "District");
+        ValidatePropertiesString(city, "City");
+        ValidatePropertiesString(stage, "Stage");
+        ValidatePropertiesInt(cep, "CEP");
+        ValidatePropertiesInt(number, "Number");
+
+
+        Road = road;
+        Number = number;
+        District = district;
+        City = city;
+        Stage = stage;
+    }
+
+    public void Update(string road, string district, string city, string stage, int cep, int number)
+    {
+
+        if (!ValidatePropertiesString(road, "Road"))
+            throw new Exception("Invalid road");
+
+        if (!ValidatePropertiesString(district, "District"))
+            throw new Exception("Invalid district");
+
+        if (!ValidatePropertiesString(city, "City"))
+            throw new Exception("Invalid city");
+
+        if (!ValidatePropertiesString(stage, "Stage"))
+            throw new Exception("Invalid stage");
+
+        if (!ValidatePropertiesInt(cep, "CEP"))
+            throw new Exception("Invalid cep");
+
+        Road = road;
+        Number = number;
+        District = district;
+        City = city;
+        Stage = stage;
     }
 }
--- WebApplication/src/Domain/Entities/DataBasic.cs
+++ WebApplication/src/Domain/Entities/DataBasic.cs
@@ -2,19 +2,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 public class DataBasic : Notification
 {
     [Column("Name")]
-    public string Name {get; set;}
+    public string Name { get; set; }
 
     [Column("Date of birth ")]
-    public DateTime DateOfBirth {get; set;}
+    public DateTime DateOfBirth { get; set; }
 
     [Column("CPF")]
-    public int Cpf {get; set;}
+    public int Cpf { get; set; }
 
     [Column("Email")]
-    public string Email {get; set;}
+    public string Email { get; set; }
 
     [Column("Phone")]
-    public int Phone {get; set;}
+    public int Phone { get; set; }
 
 
     public DataBasic(string name, DateTime dateOfBirth, int cpf, string email, int phone)
@@ -25,33 +25,33 @@ public class DataBasic : Notification
         ValidatePropertiesString(name, "name");
         ValidatePropertiesString(email, "email");
         ValidatePropertiesDateOfBirth(dateOfBirth, "date of birth");
-    
+
         Name = name;
         DateOfBirth = dateOfBirth;
         Cpf = cpf;
         Email = email;
         Phone = phone;
-        
+
     }
 
-    public void Update (string name, DateTime dateOfBirth, int cpf, string email, int phone)
+    public void Update(string name, DateTime dateOfBirth, int cpf, string email, int phone)
     {
-        
-        if(!ValidatePropertiesInt(cpf, "cpf"))
-        throw new Exception("Invalid cpf");
 
-        if(!ValidatePropertiesInt(phone, "phone"))
-        throw new Exception("Invalid phone");
+        if (!ValidatePropertiesInt(cpf, "cpf"))
+            throw new Exception("Invalid cpf");
 
-        if(!ValidatePropertiesString(name, "name"))
-        throw new Exception("Invalid name");
+        if (!ValidatePropertiesInt(phone, "phone"))
+            throw new Exception("Invalid phone");
 
-        if(!ValidatePropertiesString(email, "email"))
-        throw new Exception("Invalid email");
+        if (!ValidatePropertiesString(name, "name"))
+            throw new Exception("Invalid name");
 
-        if(!ValidatePropertiesDateOfBirth(dateOfBirth, "date of birth"))
-        throw new Exception("Invalid birth");
-        
+        if (!ValidatePropertiesString(email, "email"))
+            throw new Exception("Invalid email");
+
+        if (!ValidatePropertiesDateOfBirth(dateOfBirth, "date of birth"))
+            throw new Exception("Invalid birth");
+
         Name = name;
         DateOfBirth = dateOfBirth;
         Cpf = cpf;
