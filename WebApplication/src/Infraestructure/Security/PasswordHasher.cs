7c2f3546473717243ea629a72a4073ef10243ebd
pedroooliveira25 <eu.pedrohfo@gmail.com>
2026-06-01 12:04:34 -0300

    Criando a conversão de password para hash e definindo a logica com salt

--- WebApplication/src/Infraestructure/Security/PasswordHasher.cs
+++ WebApplication/src/Infraestructure/Security/PasswordHasher.cs
@@ -1,17 +1,24 @@
 
+using System.Security.Cryptography;
 public class PasswordHasher : IPasswordHasher
 {
     public string Hash(string password)
     {
+        byte[] salt = RandomNumberGenerator.GetBytes(16);
         
-    }
-    bool Verify(string password, string hash)
-    {
-        
-    }
+        byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, 100000, HashAlgorithmName.SHA256, 32);
 
-    bool IPasswordHasher.Verify(string password, string hash)
+        return Convert.ToBase64String(salt) + "." + Convert.ToBase64String(hash);
+    }
+      public bool Verify(string password, string storedHash)
     {
-        return Verify(password, hash);
+        var parts = storedHash.Split('.');
+
+        byte[] salt = Convert.FromBase64String(parts[0]);
+        byte[] hash = Convert.FromBase64String(parts[1]);
+
+        byte[] newHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, 100000, HashAlgorithmName.SHA256, 32);
+
+        return CryptographicOperations.FixedTimeEquals(hash, newHash);
     }
 }
